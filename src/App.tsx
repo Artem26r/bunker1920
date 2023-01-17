a956846a1576fa26f3983438370979d389638031
Artem <35739625+Artem26r@users.noreply.github.com>
2023-01-17 18:21:26 +0300

    concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { Main } from "pages/Main";
+import { Container } from "components/Container";
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <div className="overlay"></div>
+      {/* <div className="scanline"></div> */}
+      <Container>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,41 @@
+import styles from "./Header.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export const Header = () => (
+  <header>
+    <div className={styles.logo}>
+      <Logo />
+      <span>БУНКЕР1920</span>
+    </div>
+    <div className={styles.right}>
+      <div>
+        <div className={styles.rightTitie}>СИСТЕМНАЯ СЕССИЯ 412(ТМ)</div>
+        <div className={styles.abbreviation}>
+          <div>
+            <span>Б</span>ЮРО
+          </div>{" "}
+          <div>
+            <span>У</span>СЛОВНО
+          </div>{" "}
+          <div>
+            <span>Н</span>АУЧНО
+          </div> 
+          <div>{" "}
+            <span>К</span>ВАНТОВЫХ
+          </div>{" "}
+          <div>
+            <span>Е</span>ДИНЫХ
+          </div>{" "}
+          <div>
+            <span>Р</span>ЕЗУЛЬТАТОВ
+          </div>
+        </div>
+        <div className={styles.version}>ВЕРСИЯ 1.0.0</div>
+        <div className={styles.error}>ОШИБКА СИСТЕМЫ БЕЗОПАСНОСТИ</div>
+        <div className={styles.protocol}>ПРОТОКОЛ ПОДКЛЮЧЕНИЯ СТАНДАРТА В14-23КР</div>
+        <div className={styles.srever}>--- СЕРВЕР 12.423.32 (ОСТРОВ ВИЗЕ) ---</div>
+        <div className={styles.coordinates}>--- КООРДИНАТЫ 79.538164, 76.88614 ---</div>
+      </div>
+    </div>
+  </header>
+);
